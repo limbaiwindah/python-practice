a04484081694c026cdc5105956484b03672b7242
catodile <134586423+limbaiwindah@users.noreply.github.com>
2024-02-15 22:48:37 +0800

    Create dictionary-prractice.py

---
+++ dictionary-prractice.py
@@ -0,0 +1,8 @@
+# retriving name from dictionary using key to get the value
+
+# declaring dictionary
+windah_limbai = {"first": "Windah", "surname": "Limbai", "birthday": "2 September 2005"}
+
+# using dictionary to create an intro
+print("Hi, my name is " , windah_limbai["first"] + " " + windah_limbai["surname"] , " and I was born on ", windah_limbai["birthday"])  
+  
